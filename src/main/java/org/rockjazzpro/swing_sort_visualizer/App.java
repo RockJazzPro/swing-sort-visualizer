25ac8d02fd4e526f5ba94cd088ee26f8a1a950a7
RockJazzPro <nicola.studer2000@gmail.com>
2018-12-04 15:25:10 +0100

    Initialize Project

---
+++ src/main/java/org/rockjazzpro/swing_sort_visualizer/App.java
@@ -0,0 +1,13 @@
+package org.rockjazzpro.swing_sort_visualizer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
